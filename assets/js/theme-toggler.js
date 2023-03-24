5d164105e9bd180b91587bc64dac7ec20b39c37b
Michelle Mounde <mitchmounde@gmail.com>
2023-03-24 20:52:16 +0300

    feat: add aria-labels as needed & add skeleton for experience, open source contributions and recognition & edit colours of font and buttons for brightness reduction on dark theme and different coloured heading vs content for both themes

--- assets/js/theme-toggler.js
+++ assets/js/theme-toggler.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const themeToggler = document.querySelector(".theme-toggle");
   const lightTheme = document.querySelector("div.light");
   const darkTheme = document.querySelector("div.dark");
 
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("a.top svg").style.color = "#47D3E5"
 
     document.querySelector("#wrapper").style.backgroundColor = "white"
-    document.querySelector("#wrapper").style.color = "black"
+    document.querySelector("#wrapper").style.color = "#4B4D4F"
 
     document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(heading => {
       heading.style.color = "black"
@@ -23,13 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
       section.style.color = "black"
     });
 
+    document.querySelector(".name").style.color = "black"
+
     document.querySelector("footer").style.backgroundColor = "#EBEAEA"
 
     document.querySelectorAll("a").forEach(link => {
       link.style.color = "black"
     });
 
-
+    themeToggler.ariaLabel = "Change to dark mode";
+    themeToggler.title = "Change to dark mode";
   };
 
   darkTheme.onclick = function () {
@@ -38,27 +42,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll("a.button").forEach(button => {
       button.classList.remove("primary");
-      button.style.backgroundColor = "#E6FBFF"
+      button.style.backgroundColor = "#B1C1C4"
     });
     document.querySelector("a.top svg").style.color = "black"
 
 
     document.querySelector("#wrapper").style.backgroundColor = "#00252E"
-    document.querySelector("#wrapper").style.color = "white"
+    document.querySelector("#wrapper").style.color = "#E8E8E8"
 
     document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(heading => {
-      heading.style.color = "white"
+      heading.style.color = "#FCFCFC"
     });
 
     document.querySelectorAll("nav, footer").forEach(section => {
-      section.style.color = "white"
+      section.style.color = "#FCFCFC"
     });
+
+    document.querySelector(".name").style.color = "#FCFCFC"
 
     document.querySelector("footer").style.backgroundColor = "#004252"
 
     document.querySelectorAll("a").forEach(link => {
-      link.style.color = "white"
+      link.style.color = "#FCFCFC"
     });
 
+    themeToggler.ariaLabel = "Change to light mode";
+    themeToggler.title = "Change to light mode";
   };
+
 })
